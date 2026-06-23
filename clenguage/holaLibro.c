17071f0213bf7d4c83a8d59ec9483562b4a73a77
SunnySuaSal <yo_soyalegre@hotmail.com>
2026-06-23 16:05:03 -0600

    first programs of the book

---
+++ clenguage/holaLibro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  printf("hello, n\n");
+  printf("hello, t\t");
+printf("hello, b\b");
+printf("hello, world\\");
+printf("\"hello, world\"");
+  printf("hello world /g");
+}
