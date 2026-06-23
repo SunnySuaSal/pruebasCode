7510e2e362ac1dfc927e99e7c77a64f2f4fd86f2
SunnySuaSal <yo_soyalegre@hotmail.com>
2026-06-23 16:07:16 -0600

    uses width for prettyer formatting

--- clenguage/fahToCelConvInt.c
+++ clenguage/fahToCelConvInt.c
@@ -13,7 +13,7 @@ int main(){
   fahr = lower;
   while (fahr <= upper) {
     celsius = 5 * (fahr - 32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    printf("%3d\t%6d\n", fahr, celsius); //lol, finda like formating in Fortran
     fahr += step;
   }
   return 0;
