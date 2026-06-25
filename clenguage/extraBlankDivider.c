4658c6ed1e166de38416c69a7ed838cbcf812cd0
SunnySuaSal <yo_soyalegre@hotmail.com>
2026-06-25 10:33:41 -0600

    sanitizes input and divides words in lines

---
+++ clenguage/extraBlankDivider.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* Apparently there is a 
+ * improved version of my extra blanks remover
+ * that uses states instead of checking the next character
+ * in roder to avoid the risk of losing characters */
+int main(){
+  int c, prevWasBlank;
+  prevWasBlank = 0; //starts false
+
+  while ((c = getchar()) != EOF) {
+    if(c == ' '){ //if received an space
+      if(!prevWasBlank){ //if previous was NOT an space
+        putchar('\n'); //Aqui es el espacio muajajaja
+        prevWasBlank = 1; //coz now we now prev was indeed a blank
+      }
+    }else{
+      putchar(c);
+      prevWasBlank = 0;
+    }
+  }
+
+  return 0;
+}
