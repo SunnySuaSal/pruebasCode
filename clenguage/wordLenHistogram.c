e7a319d3296a39b964520a99930a42da0780427e
SunnySuaSal <yo_soyalegre@hotmail.com>
2026-06-25 13:48:05 -0600

    Histogram of words in input complete, I am a genius

--- clenguage/wordLenHistogram.c
+++ clenguage/wordLenHistogram.c
@@ -5,19 +5,27 @@
 
 /* Prints a histogram of lenght of words in input*/
 int main(){
-  int c, nc, nw, state;
+  int c, nc, nw, state, wl;
   state = OUT;
-  nw = nc = 0;
+  wl = nw = nc = 0;
   while ((c = getchar()) != EOF) {
     ++nc;
+    ++wl;
     if(c == ' '||c == '\n'|| c == '\t'){
       --nc;
+      --wl;
       state = OUT;
+      //here, add a new item in lenArray and store wl
+      //no need, I can just do it here
+      //printf("len:%d ", wl);
+      for(int j = 0; j < wl; ++j)
+        putchar('#');
+      wl = 0;
+      putchar('\n');
     }
     else if(state == OUT){
       state = IN;
-      ++nw;
-      //here add an item in the lenghts array
+      ++nw; 
     }
   }
   printf("characters: %d\nwords: %d\n", nc, nw);
