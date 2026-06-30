9b5e7c5aca7408adc89b129884667dcf03754ba6
SunnySuaSal <yo_soyalegre@hotmail.com>
2026-06-30 15:26:49 -0600

    exercise 1-16: keeps counting length even if buffer size exceeded

--- clenguage/printLongestLIne.c
+++ clenguage/printLongestLIne.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAXLINE 1000 /* Maximum input line size */
+#define MAXLINE 16 /* Maximum input line size */
 
 int getLine(char line[], int maxLine);
 void copy(char to[], char from[]);
@@ -17,22 +17,32 @@ int main(){
       max = len;
       copy(longest, line);
     }
-    if(max > 0) //there was a line
-      printf("%s\n", longest);
   }
+  if(max > 0) //there was a line
+      printf("%s\n", longest);
 
   return 0;
 }
 
 /* getline: read a line into s, return length */
 int getLine(char s[], int lim){
-  int c, i;
-  for(i=0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
-    s[i] = c;
-  if(c == '\n'){
-    s[i] = c;
-    ++i; //necesito entender esto
+  int c, i, len;
+  i = len = 0;
+  //for(i=0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+  while((c = getchar()) != EOF && c != '\n'){
+    if(i < lim-1){ //if there is space
+      s[i++] = c;
+    }
+    ++len;
   }
+  //store the newline if there is room
+  if(c == '\n'){
+    if(i < lim-1)
+      s[i++] = c;
+    ++len;
+  }
+
+  //remember always the sring terminator at the end
   s[i] = '\0';
   return i;
 }
